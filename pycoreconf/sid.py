80ae6a31c12ac2036bb9096cb000c2e2398a4a32
alex-fddz <javier.fddz@gmail.com>
2023-03-09 15:56:43 +0100

    Make ModelSID class

--- pycoreconf/sid.py
+++ pycoreconf/sid.py
@@ -1,79 +1,66 @@
 import json
 
-def getIdentifiers(file):
+class ModelSID:
     """
-    Read SID file and return { sid : identifier } dictionary.
+    Class to define methods for reading a YANG model SID file and hold values.
     """
-    # Read the contents of the sid/json file
-    f = open(file, "r")
-    obj = json.load(f)
-    f.close()
 
-    # Get items & map identifier : sid
-    ids = {} # init
-    items = obj["items"] # list
-    for item in items:
-        ids[item["sid"]] = item["identifier"]
+    def __init__(self, sid_file):
+        self.sid_file = sid_file
+        
 
-    return ids
+    def getIdentifiers(self):
+        """
+        Read SID file and return { sid : identifier } dictionary.
+        """
+        # Read the contents of the sid/json file
+        f = open(self.sid_file, "r")
+        obj = json.load(f)
+        f.close()
 
+        # Get items & map identifier : sid
+        ids = {} # init
+        items = obj["items"] # list
+        for item in items:
+            ids[item["sid"]] = item["identifier"]
 
-def getSIDsAndTypes(file):
-    """
-    Read SID file and return { identifier : sid } + { identifier : type } dictionaries.
-    """
-    # Read the contents of the sid/json file
-    f = open(file, "r")
-    obj = json.load(f)
-    f.close()
-
-    # Get items & map identifier : sid and leafIdentifier : typename
-    sids = {} # init
-    types = {} # init
-    items = obj["items"] # list
-    for item in items:
-        sids[item["identifier"]] = item["sid"]
-        if "type" in item.keys():
-            types[item["identifier"]] = item["type"]
+        return ids
 
 
-    return sids, types
+    def getSIDsAndTypes(self):
+        """
+        Read SID file and return { identifier : sid } + { identifier : type } dictionaries.
+        """
+        # Read the contents of the sid/json file
+        f = open(self.sid_file, "r")
+        obj = json.load(f)
+        f.close()
 
-def getSIDs(file):
-    """
-    Read SID file and return { identifier : sid } dictionary.
-    """
-    # Read the contents of the sid/json file
-    f = open(file, "r")
-    obj = json.load(f)
-    f.close()
-
-    # Get items & map identifier : sid
-    sids = {} # init
-    items = obj["items"] # list
-    for item in items:
-        sids[item["identifier"]] = item["sid"]
-
-    return sids
+        # Get items & map identifier : sid and leafIdentifier : typename
+        sids = {} # init
+        types = {} # init
+        items = obj["items"] # list
+        for item in items:
+            sids[item["identifier"]] = item["sid"]
+            if "type" in item.keys():
+                types[item["identifier"]] = item["type"]
 
 
-def main():
-    import sys
+        return sids, types
 
-    # Take specified file (arg)
-    try:
-        file = sys.argv[1]
-    except IndexError:
-        raise SystemExit(f"Usage: {sys.argv[0]} <file.sid>")
+    def getSIDs(self):
+        """
+        Read SID file and return { identifier : sid } dictionary.
+        """
+        # Read the contents of the sid/json file
+        f = open(self.sid_file, "r")
+        obj = json.load(f)
+        f.close()
 
-    import pprint
-    pp = pprint.PrettyPrinter(indent=2)
+        # Get items & map identifier : sid
+        sids = {} # init
+        items = obj["items"] # list
+        for item in items:
+            sids[item["identifier"]] = item["sid"]
 
-    # Convert and pretty-print
-    print(file)
-    sids, types = getSIDsAndTypes(file)
-    pp.pprint(sids)
-    pp.pprint(types)
-
-if __name__ == "__main__":
-    main()
+        return sids
