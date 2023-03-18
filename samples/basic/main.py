fcd7211413a936e156bce410413aa4a6f43e3406
alex-fddz <javier.fddz@gmail.com>
2023-03-18 15:45:12 +0100

    Add samples/basic

---
+++ samples/basic/main.py
@@ -0,0 +1,23 @@
+# pycoreconf sample: "basic"
+# This script demonstrates the basic usage of pycoreconf
+#  using a simple YANG datamodel.
+
+import pycoreconf
+
+# Create the model object
+ccm = pycoreconf.CORECONFModel("example-1@unknown.sid")
+
+# Read JSON configuration file
+config_file = "ex1-config.json"
+
+with open(config_file, "r") as f:
+    json_data = f.read()
+print("Input JSON config data =\n", json_data, sep='')
+
+# Convert configuration to CORECONF/CBOR
+cbor_data = ccm.toCORECONF(json_data) # can also take json_data
+print("Encoded CBOR data (CORECONF payload) =", cbor_data)
+
+# Decode CBOR data back to JSON configuration data
+decoded_json = ccm.toJSON(cbor_data)
+print("Decoded config data =", decoded_json)
