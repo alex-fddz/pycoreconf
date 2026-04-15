efacd8274af991677a4e80308f832b16649dd959
Javier Alejandro FERNANDEZ <javier.fddz@gmail.com>
2026-04-15 17:31:55 +0200

    sample/multisid: remove validation for simplicity

--- samples/multisid/main.py
+++ samples/multisid/main.py
@@ -5,13 +5,13 @@
 import pycoreconf
 import json
 
-# Create the model object (specify .sid and model description json file)
-ccm = pycoreconf.CORECONFModel(["ietf-schc@2023-01-28.sid",
-                                "ietf-schc-oam@2021-11-10.sid"],
-                               model_description_file="description.json")
-# Specify modules location for validation (or a list of paths):
-ccm.add_modules_path("/home/alex/Projects/pyang/modules/ietf/")
-ccm.add_modules_path(["/path/to/modules/", "another/path/"])
+# Create the model object with multiple SID files
+ccm = pycoreconf.CORECONFModel(
+    [
+        "ietf-schc@2023-01-28.sid",
+        "ietf-schc-oam@2021-11-10.sid"
+    ]
+)
 
 # Read JSON configuration file
 config_file = "schc.json"
@@ -22,7 +22,7 @@ print(f"Encoded CBOR data (CORECONF payload) = {cbor_data.hex()}" \
     f" ({len(cbor_data)})")
 
 # Decode CBOR data back to JSON configuration data
-decoded_json = ccm.toJSON(cbor_data)  # will validate the decoded config
+decoded_json = ccm.toJSON(cbor_data)
 # print("\nDecoded config data =", decoded_json, "\n")
 
 # Test
